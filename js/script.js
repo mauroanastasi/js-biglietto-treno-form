1d647a99299353295ba03447484f95827af27e6f
mauroanastasi <mauroanastasi1@gmail.com>
2025-03-26 16:54:53 +0100

    [UPD]problema risolto, MILESTONE 1 completato

--- js/script.js
+++ js/script.js
@@ -1,27 +1,31 @@
 
 // Recupero elementi DOM e inizializzo variabili necessarie
 
-const age = document.getElementById(`mettiEtà`);
+const age = document.getElementById(`mettiEta`);
 const km = document.getElementById(`mettiKm`);
 const button = document.querySelector(`.pulsanteInvia`)
-let prezzo = km.value * 0.21;
 let message;
+let prezzo;
 
 // Corpo del programma
 button.addEventListener(`click`, function(){
-if(km.value < 18){
-    prezzo= prezzo *0.8
+    prezzo = km.value * 0.21;
+
+if(age.value < 18){
+
+    prezzo*= 0.8;
     message= `hai diritto ad uno sconto del 20% il prezzo è di ${prezzo.toFixed(2)}`
+    console.log (message)
 }
 else if(age.value > 65){
-    prezzo= prezzo *0.6
+
+    prezzo*= 0.6;
     message= `hai diritto ad uno sconto del 40% il prezzo è di ${prezzo.toFixed(2)}`
+    console.log (message)
 }
 else{
-    prezzo= prezzo *0.8
-    message= `hai diritto ad uno sconto del 20% il prezzo è di ${prezzo.toFixed(2)}`
+    message= `il prezzo è di ${prezzo.toFixed(2)}`
+    console.log (message)
 }
 })
 // Risultato finale
-
-console.log (message)
